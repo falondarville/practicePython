21163af064f3b9142b8c9ac8f9cb7f524ae03cbd
Falon Darville <falondarville@gmail.com>
2018-07-16 11:45:35 -0700

    list comprehension exercise

---
+++ listComprehension.py
@@ -0,0 +1,6 @@
+# Write one line of Python that takes this list a and makes a new list that has only the even elements of this list in it.
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+evens = [n for n in a if n % 2 == 0]
+
+print(evens)
