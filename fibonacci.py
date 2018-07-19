01956d7a6f7d6e47cf083a976c14155e3116f7ae
Falon Darville <falondarville@gmail.com>
2018-07-19 13:05:43 -0700

    start fibonacci exercise

---
+++ fibonacci.py
@@ -0,0 +1,11 @@
+# Write a program that asks the user how many Fibonnaci numbers to generate and then generates them.
+# The Fibonnaci seqence is a sequence of numbers where the next number in the sequence is the sum of the previous two numbers in the sequence. The sequence looks like this: 1, 1, 2, 3, 5, 8, 13, …
+print("How many Fibonnaci sequence numbers would you like to generate?")
+
+user_input = int(input())
+
+def check_fibonacci(num):
+	# generate the sequence on the fly or hard code it?
+	# take the user_input, which will be a single number, and use this to generate the number of numbers that will return. 
+
+print(check_fibonacci(user_input))
