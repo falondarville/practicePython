41a8aaafd7947f51c7d7167c7da751993880256b
Falon Darville <falondarville@gmail.com>
2018-07-22 00:06:57 -0700

    add beautiful soup starter code

--- decode/decode.py
+++ decode/decode.py
@@ -3,7 +3,11 @@
 
 # Note that I had to install this package using pip3, not pip.
 import requests
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html_doc, 'html.parser')
 
 r = requests.get('https://www.nytimes.com/')
+
+soup.h1["story-heading"].a
 
 print(r)
