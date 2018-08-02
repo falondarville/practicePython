74b564793956a2f3d386315eb73ae816693d93e6
Falon Darville <falondarville@gmail.com>
2018-08-01 17:51:35 -0700

    additions to guess game, not complete

--- SecondGuessGame.py
+++ SecondGuessGame.py
@@ -10,32 +10,52 @@ user_input = input()
 still_playing = True
 num_guesses = 0
 
+# need to change the variables so that I am not finding the max and min, but cutting off options as the window of guesses narrows
 if user_input == "go":
 	while still_playing:
 		all_guesses = []
-		high_num = max(all_guesses)
-		low_num = min(all_guesses)
-		computer_guess = randint(0, 101)
-		print(f"Is {computer_guess} your number?")
+		high_num = None
+		low_num = None
+		first_computer_guess = randint(0, 101)
+		print(f"Is {first_computer_guess} your number?")
 		print("Please enter one of the following: 'too high', 'too low', 'you got it'")
 		response = input()
+		all_guesses.append(first_computer_guess)
 		if response == "too high":
-			if high_num.length > 0 and low_num.length > 0: 
+			if high_num and low_num: 
 				computer_guess = randint(low_num, high_num)
-			else: 
-				computer_guess = randint(0, computer_guess)
+				if computer_guess > high_num:
+					high_num = computer_guess
+				elif computer_guess < low_num:
+					low_num = computer_guess
+			elif low_num == None and high_num:
+				computer_guess = randint(0, high_num)
+			elif high_num == None and low_num:
+				computer_guess = randint(low_num, 101)
+			else:
+				computer_guess = randint(0, first_computer_guess)
 			all_guesses.append(computer_guess)
 			print(f"Is {computer_guess} your number?")
 			print("Please enter one of the following: 'too high', 'too low', 'you got it!'")
+			response = input()
 			num_guesses += num_guesses
 		if response == "too low":
-			if high_num.length > 0 and low_num.length > 0: 
+			if high_num and low_num: 
 				computer_guess = randint(low_num, high_num)
+				if computer_guess > high_num:
+					high_num = computer_guess
+				elif computer_guess < low_num:
+					low_num = computer_guess
+			elif low_num == None and high_num:
+				computer_guess = randint(0, high_num)
+			elif high_num == None and low_num:
+				computer_guess = randint(low_num, 101)
 			else: 
-				computer_guess = randint(computer_guess, 101)
-			all_guesses.append(all_guesses)
+				computer_guess = randint(first_computer_guess, 101)
+			all_guesses.append(computer_guess)
 			print(f"Is {computer_guess} your number?")
 			print("Please enter one of the following: 'too high', 'too low', 'you got it!'")
+			response = input()
 			num_guesses += num_guesses
 		if response == "you got it":
 			num_guesses += num_guesses
